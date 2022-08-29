d1cbc979cd64a5ed50fa6cd82f68f234d2ff59f5
kjain1810 <kunaljain.2000@yahoo.in>
2022-08-29 23:39:27 +0530

    kya kar rhe hai hum

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.8.16;
 
+import { FirstAuction } from "./auction.sol";
+
 contract Marketplace {
 
     enum AuctionState {RUNNING, NOT_STARTED, FINISHED}
@@ -12,41 +14,69 @@ contract Marketplace {
     mapping (uint => Manufacturer) public manufacturers;
     mapping (uint => Customer) public customers;
 
+    address payable public owner;
+
     constructor() {
+        owner = payable(msg.sender);
         num_manufacturer = 0;
         num_supplier = 0;                
     }
 
     struct Supplier {
-        int tag;
+        uint tag;
         int partType;
         uint quantityAvailable;
         AuctionState currentState;
+        address wallet;
+        uint maxBidders;
+        FirstAuction auction;
     }
 
     struct Manufacturer {
-        int _tag;
+        uint _tag;
         uint quantityA;
         uint quantityB;
         AuctionState currentState;
+        address wallet;
+        uint maxBidders;
     }
 
     struct Customer {
-        int _tag;
+        uint _tag;
+        address wallet;
     }
 
-    function addSupplier(int tag, int partType, uint quantityAvailable) public {
+    // function makeBidSupplier(int index, ...) public {
+    //     suppliers[index].auction.makeBid(...);
+    // }
+
+    function addSupplier(int partType, uint quantityAvailable, address payable addr, uint auctionBidders) public {
         num_supplier++;
-        suppliers[num_supplier] = Supplier(tag, partType, quantityAvailable, AuctionState.NOT_STARTED);
+        suppliers[num_supplier] = Supplier(num_supplier, partType, quantityAvailable, AuctionState.NOT_STARTED, addr, auctionBidders);
     }
 
-    function addManufacturer(int tag) public {
+    function addManufacturer(address payable addr, uint auctionBidders) public {
         num_manufacturer++;
-        manufacturers[num_manufacturer] = Manufacturer(tag, 0, 0, AuctionState.NOT_STARTED);
+        manufacturers[num_manufacturer] = Manufacturer(num_manufacturer, 0, 0, AuctionState.NOT_STARTED, addr, auctionBidders);
     }
 
-    function addCustomer(int tag) public {
+    function addCustomer(address payable addr) public {
         num_customer++;
-        customers[num_customer] = Customer(tag);
+        customers[num_customer] = Customer(num_customer, addr);
     }
+
+    event StartSupplierAuction(int supplierID, int endTime);
+    event EndSupplierAuction(int supplierID, int endTime);
+
+    event StartManufacturerAuction(int manufacturerID, int endTime);
+    event EndManufacturerAuction(int manufacturerID, int endTime);
+    
+    // Manufacturer places a bid to the supplier
+    event ManufacturerBids(int supplierID, int manufacturerID, bytes32 blindBid);
+    // Manufacturer reveals it's bid to the supplier by providing the key
+    event ManufacturerReveal();
+    
+    // Customer places a bid to the manufacturer
+    event CustomerBid(int manufacturerID, int customerID, bytes32 blindBid);
+    event CustomerReveal();
 }

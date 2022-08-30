9add4110c8450485a414d4039900edf75c548699
kjain1810 <kunaljain.2000@yahoo.in>
2022-08-30 20:00:57 +0530

    Understanding shit

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,20 +6,33 @@ contract Marketplace {
 
     enum AuctionState {RUNNING, NOT_STARTED, FINISHED}
 
-    uint num_supplier;
-    uint num_manufacturer;
-    uint num_customer;
+    struct Bid {
+        uint256 valuePrice;
+        uint256 valueQuantity;
+        address payable bidderAddress;
+        bytes32 blindBidPrice; // will have price and quantity you are bidding  
+        bytes32 blindBidQuantity;
+        uint limitingResourceQuantity;
+        uint buyerID;
+        uint sellerID;
+    }
+
+    uint public num_supplier;
+    uint public num_manufacturer;
+    uint public num_customer;
 
     mapping (uint => Supplier) public suppliers;
     mapping (uint => Manufacturer) public manufacturers;
     mapping (uint => Customer) public customers;
+    mapping (address => Bid) bidsTillNow;
 
     address payable public owner;
 
     constructor() {
         owner = payable(msg.sender);
         num_manufacturer = 0;
-        num_supplier = 0;                
+        num_supplier = 0;
+        num_customer = 0;                
     }
 
     struct Supplier {
@@ -29,7 +42,6 @@ contract Marketplace {
         AuctionState currentState;
         address wallet;
         uint maxBidders;
-        FirstAuction auction;
     }
 
     struct Manufacturer {
@@ -50,6 +62,38 @@ contract Marketplace {
     //     suppliers[index].auction.makeBid(...);
     // }
 
+    function supplierStartAuction (uint tag) public {
+        // function for supplier to start their bidding process
+    }
+
+    function supplierEndAuction (uint tag) public {
+        // function for supplier to end their auctin
+    }
+
+    function manufacturerPlacesBid(uint manufacturerID, uint supplierID) public {
+        // function for manufacturer to place a bid
+    }
+
+    function customerPlacesBid(uint manufacturerID, uint supplierID) public {
+        // function for customer to place a bid        
+    }
+
+    function manufactuerRevealBid(uint manufacturerID) public {
+
+    }
+
+    function customerRevealBid(uint manufacturerID) public {
+        
+    }
+
+    function allBidsPlacedForSupplier(uint suppliedID) private {
+        // once all bids have been placed, decide how much goes to each bidder
+    }
+
+    function allBidsPlacedForManufacturer(uint manufacturerID) private {
+        // once all bids have been placed, decide how much goes to each bidder
+    }
+
     function addSupplier(int partType, uint quantityAvailable, address payable addr, uint auctionBidders) public {
         num_supplier++;
         suppliers[num_supplier] = Supplier(num_supplier, partType, quantityAvailable, AuctionState.NOT_STARTED, addr, auctionBidders);
@@ -63,6 +107,12 @@ contract Marketplace {
     function addCustomer(address payable addr) public {
         num_customer++;
         customers[num_customer] = Customer(num_customer, addr);
+    }
+
+    // Setter functions
+    function supplierAddQuantity(uint suppliedID, uint quantityToAdd) public view {
+        require(suppliers[suppliedID].wallet == msg.sender);
+        // increase the supplier quantity
     }
 
     event StartSupplierAuction(int supplierID, int endTime);
@@ -79,4 +129,14 @@ contract Marketplace {
     // Customer places a bid to the manufacturer
     event CustomerBid(int manufacturerID, int customerID, bytes32 blindBid);
     event CustomerReveal();
+
+
+
+    // ALL GET FUNCTIONS
+    function getCustomers() public view returns (address[200] memory){
+        address[200] memory ret;
+        for (uint i = 1; i <= num_customer; i+=1)
+            ret[i] = customers[i].wallet;
+        return ret;        
+    }
 }
